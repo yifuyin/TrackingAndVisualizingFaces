481ec5060ed68bd8a5c718883d84c8b9cc3749bf
yifuyin <yifuyin7@gmail.com>
2023-05-10 22:01:34 -0700

    first commit

--- ARKitFaceExample/ViewController.swift
+++ ARKitFaceExample/ViewController.swift
@@ -1,167 +1,64 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Main view controller for the AR experience.
-*/
-
-import ARKit
-import SceneKit
 import UIKit
+import ARKit
+import SpriteKit
+import openLiveAvatarSDK
 
-class ViewController: UIViewController, ARSessionDelegate {
-    
-    // MARK: Outlets
-
-    @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var tabBar: UITabBar!
-
-    // MARK: Properties
-    
-    var faceAnchorsAndContentControllers: [ARFaceAnchor: VirtualContentController] = [:]
-    
-    var selectedVirtualContent: VirtualContentType! {
-        didSet {
-            guard oldValue != nil, oldValue != selectedVirtualContent
-                else { return }
-            
-            // Remove existing content when switching types.
-            for contentController in faceAnchorsAndContentControllers.values {
-                contentController.contentNode?.removeFromParentNode()
-            }
-            
-            // If there are anchors already (switching content), create new controllers and generate updated content.
-            // Otherwise, the content controller will place it in `renderer(_:didAdd:for:)`.
-            for anchor in faceAnchorsAndContentControllers.keys {
-                let contentController = selectedVirtualContent.makeController()
-                if let node = sceneView.node(for: anchor),
-                let contentNode = contentController.renderer(sceneView, nodeFor: anchor) {
-                    node.addChildNode(contentNode)
-                    faceAnchorsAndContentControllers[anchor] = contentController
-                }
-            }
-        }
-    }
-    
-    // MARK: - View Controller Life Cycle
+class ViewController: UIViewController, ARSCNViewDelegate {
+    var testVc: LiveAvatarController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        sceneView.delegate = self
-        sceneView.session.delegate = self
-        sceneView.automaticallyUpdatesLighting = true
-        
-        // Set the initial face content.
-        tabBar.selectedItem = tabBar.items!.first!
-        selectedVirtualContent = VirtualContentType(rawValue: tabBar.selectedItem!.tag)
+        testVc = LiveAvatarController(apiKey: "8AGVCQ.4ZbMQw:-tzSxyQZzuY6OGs5JlHSOeqyHeWB7Agf0_e6YYVHbIw", channelName: "avatar-state");
+
+//        // Set the view's delegate
+//        sceneView.delegate = self
+//
+//        // Create a SpriteKit scene for the animated face and present it
+//        faceScene = AnimatedFaceScene(size: view.bounds.size)
+//        let skView = SKView(frame: view.bounds)
+//        skView.presentScene(faceScene)
+//        view.addSubview(skView)
     }
 
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        // Create a session configuration
+//        let configuration = ARFaceTrackingConfiguration()
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
+//    }
+    
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        // AR experiences typically involve moving the device without
-        // touch input for some time, so prevent auto screen dimming.
-        UIApplication.shared.isIdleTimerDisabled = true
-        
-        // "Reset" to run the AR session for the first time.
-        resetTracking()
+        guard let testVc = testVc else { return }
+        testVc.startCapture()
+        var sceneView = testVc.skView
+
+        // Add it to parents subview
+        self.view.addSubview(sceneView)
+        // self.view.addSubview(testVc.skView)
+
+        //add autolayout contstraints
+        sceneView.translatesAutoresizingMaskIntoConstraints = false
+        sceneView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        sceneView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        sceneView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        sceneView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
 
-    // MARK: - ARSessionDelegate
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//
+//        // Pause the view's session
+//        sceneView.session.pause()
+//    }
 
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        guard error is ARError else { return }
-        
-        let errorWithInfo = error as NSError
-        let messages = [
-            errorWithInfo.localizedDescription,
-            errorWithInfo.localizedFailureReason,
-            errorWithInfo.localizedRecoverySuggestion
-        ]
-        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-        
-        DispatchQueue.main.async {
-            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
-        }
-    }
-    
-    /// - Tag: ARFaceTrackingSetup
-    func resetTracking() {
-        guard ARFaceTrackingConfiguration.isSupported else { return }
-        let configuration = ARFaceTrackingConfiguration()
-        if #available(iOS 13.0, *) {
-            configuration.maximumNumberOfTrackedFaces = ARFaceTrackingConfiguration.supportedNumberOfTrackedFaces
-        }
-        configuration.isLightEstimationEnabled = true
-        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-        
-        faceAnchorsAndContentControllers.removeAll()
-    }
-    
-    // MARK: - Error handling
-    
-    func displayErrorMessage(title: String, message: String) {
-        // Present an alert informing about the error that has occurred.
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
-            alertController.dismiss(animated: true, completion: nil)
-            self.resetTracking()
-        }
-        alertController.addAction(restartAction)
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    // Auto-hide the home indicator to maximize immersion in AR experiences.
-    override var prefersHomeIndicatorAutoHidden: Bool {
-        return true
-    }
-    
-    // Hide the status bar to maximize immersion in AR experiences.
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+//    // ARSCNViewDelegate method
+//    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+//        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+//
+//        // Update the face components in the SpriteKit scene with the new blend shape coefficients
+//        faceScene?.updateFaceComponents(withBlendShapes: faceAnchor.blendShapes)
+//    }
 }
-
-extension ViewController: UITabBarDelegate {
-    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        guard let contentType = VirtualContentType(rawValue: item.tag)
-            else { fatalError("unexpected virtual content tag") }
-        selectedVirtualContent = contentType
-    }
-}
-
-extension ViewController: ARSCNViewDelegate {
-        
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        
-        // If this is the first time with this anchor, get the controller to create content.
-        // Otherwise (switching content), will change content when setting `selectedVirtualContent`.
-        DispatchQueue.main.async {
-            let contentController = self.selectedVirtualContent.makeController()
-            if node.childNodes.isEmpty, let contentNode = contentController.renderer(renderer, nodeFor: faceAnchor) {
-                node.addChildNode(contentNode)
-                self.faceAnchorsAndContentControllers[faceAnchor] = contentController
-            }
-        }
-    }
-    
-    /// - Tag: ARFaceGeometryUpdate
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor,
-            let contentController = faceAnchorsAndContentControllers[faceAnchor],
-            let contentNode = contentController.contentNode else {
-            return
-        }
-        
-        contentController.renderer(renderer, didUpdate: contentNode, for: anchor)
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        
-        faceAnchorsAndContentControllers[faceAnchor] = nil
-    }
-}
-
